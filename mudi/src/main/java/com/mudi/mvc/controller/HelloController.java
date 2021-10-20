1d8ee4b0c2820d13d01d12b6527b4d913a57ff57
satells <msergiost@hotmail.com>
2021-10-20 15:26:59 -0300

    lkjfdças

---
+++ mudi/src/main/java/com/mudi/mvc/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.mudi.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("nome", "oiiiiiii");
+
+		return "hello";
+	}
+
+}
